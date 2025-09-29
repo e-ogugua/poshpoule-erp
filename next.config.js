418311d5cc9ccedae4c88abddd4ec24f90d3fa1e
Emmanuel Ogugua <emmachuka@gmail.com>
2025-09-29 15:47:52 +0100

    Autosave: 2025-09-29 15:47:52

--- next.config.js
+++ next.config.js
@@ -12,19 +12,6 @@ const nextConfig = {
     ],
     unoptimized: process.env.NODE_ENV === 'development',
   },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https: http://localhost:3000; connect-src 'self';",
-          },
-        ],
-      },
-    ];
-  },
 }
 
 module.exports = nextConfig
